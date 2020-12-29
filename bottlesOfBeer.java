2f9c9a54b20d9e7699a8da9556e5709fa8a4002c
Gianni LaMartina <68284481+giannilamartina@users.noreply.github.com>
2020-12-29 08:26:58 -0600

    Create bottlesOfBeer.java

---
+++ bottlesOfBeer.java
@@ -0,0 +1,29 @@
+
+public class bottlesOfBeer {
+  public static void main (String[] args) {
+    int beerNum = 99;
+    String word = "bottles";
+
+    while (beerNum > 0) {
+      if (beerNum == 1) {
+        word = "bottle"; //ONE bottle
+      }
+
+      System.out.println(beerNum + " " + word + " of beer on the wall");
+      System.out.println(beerNum + " " + word + " of beer");
+      System.out.println("Take one down");
+      System.out.println("Pass it around");
+      beerNum--;
+      if (beerNum > 1) {
+      System.out.println(beerNum + " bottles of beer on the wall");
+      }
+      else {
+        word = "bottle";
+      }
+      if (beerNum == 0) {
+        System.out.println("No more bottles of beer on the wall!");
+      }
+    }
+  }
+
+}
